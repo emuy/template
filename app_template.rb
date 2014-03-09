d82b9a5c8ba4a69061e7abcc8dcfe28796d970c6
Nakano Ryo <nakano.kcv@gmail.com>
2014-03-09 11:49:34 +0900

    gem add add

--- app_template.rb
+++ app_template.rb
@@ -7,6 +7,39 @@ gem 'jquery-turbolinks'
 # CSS Support
 gem 'less-rails'
 
+# Haml
+gem 'haml-rails'
+
+# Pagenation
+gem 'kaminari'
+
+group :development do
+  # Converter erb => haml
+  gem 'erb2haml'
+end
+
+group :development, :test do
+  # Railsコンソールの多機能版
+  gem 'pry-rails'
+
+  # pryの入力に色付け
+  gem 'pry-coolline'
+
+  # デバッカー
+  gem 'pry-byebug'
+
+  # Pryでの便利コマンド
+  gem 'pry-doc'
+
+  # PryでのSQLの結果を綺麗に表示
+  gem 'hirb'
+  gem 'hirb-unicode'
+
+  # pryの色付けをしてくれる
+  gem 'awesome_print'
+
+end
+
 CODE
 
 # install gems
