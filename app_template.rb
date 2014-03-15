09de49d73a61486c668c4717ab79dd2b2099b86e
Nakano Ryo <nakano.kcv@gmail.com>
2014-03-15 11:07:09 +0900

    add seed ,comment out bootstrap

--- app_template.rb
+++ app_template.rb
@@ -9,10 +9,10 @@ gem 'jquery-turbolinks'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # use twitter-bootstrap-rails
-gem 'therubyracer', platforms: :ruby
+#gem 'therubyracer', platforms: :ruby
 
 # CSS Support
-gem 'less-rails'
+#gem 'less-rails'
 
 # Haml
 gem 'haml-rails'
@@ -25,7 +25,7 @@ gem 'kaminari'
 
 gem 'simple_form'
 
-gem 'twitter-bootstrap-rails'
+#gem 'twitter-bootstrap-rails'
 
 # HTML5 Validator
 gem 'html5_validators'
@@ -73,6 +73,10 @@ end
 
 CODE
 
+if yes?("This app will be following up on both the smartphone and PC?")
+  gem 'jpmobile'
+end
+
 # install gems
 run 'bundle install'
 
@@ -116,14 +120,15 @@ run 'wget https://raw.github.com/emuy/template/master/locales/ja.yml -P config/l
 run 'rake haml:replace_erbs'
 
 # Bootstrap/Bootswach/Font-Awaresome
-insert_into_file 'app/views/layouts/application.html.haml',%(
-%script{:src=>'//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js'}
-%link{:href=>'//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', :rel=>'stylesheet'}
-%link{:href=>'//netdna.bootstrapcdn.com/bootswatch/3.0.3/simplex/bootstrap.min.css', :rel=>'stylesheet'}
-), after: '= csrf_meta_tags'
+#insert_into_file 'app/views/layouts/application.html.haml',%(
+#%script{:src=>'//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js'}
+#%link{:href=>'//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', :rel=>'stylesheet'}
+#%link{:href=>'//netdna.bootstrapcdn.com/bootswatch/3.0.3/simplex/bootstrap.min.css', :rel=>'stylesheet'}
+#), after: '= csrf_meta_tags'
 
 # Simple Form
-generate 'simple_form:install --bootstrap'
+#generate 'simple_form:install --bootstrap'
+generate 'simple_form:install'
 
 # Rspec/Spring/Guard
 # ----------------------------------------------------------------
@@ -160,6 +165,65 @@ run 'sudo chmod a+x bin/spring'
 # Guard
 run 'bundle exec guard init'
 gsub_file 'Guardfile', 'guard :rspec do', "guard :rspec, cmd: 'spring rspec -f doc' do"
+
+# Setting seed
+# ==================================================
+run "ehco db/seeds.rb"
+
+run "mkdir -p db/seeds/production"
+run "mkdir -p db/seeds/development"
+run "mkdir -p db/seeds/test"
+
+run "cat << EOF > db/seeds.rb
+# -*- coding: utf-8 -*-
+require 'csv'
+
+table_names = %w()
+
+def data_entry(file_path)
+  #モデル名に変換
+  model_name = File::basename(file_path).sub(/.csv$/,"").singularize.camelize
+  puts file_path
+  header = nil
+
+  CSV.open(file_path,'r').each do |row|
+    if header.nil?
+      header = row
+      # throw Exception.new('ID列が存在しません') unless header[0] == 'id'
+      next
+    end
+    model = eval(model_name)
+    record = model.new
+
+    if Rails.env.to_s == 'development'
+      record.send('id=',dev_count)
+    end
+
+    header.each_with_index do |column,index|
+      record.send(\"\#{column}=\",row[index]) unless row[index].nil?
+    end
+    record.save!
+  end
+end
+
+ActiveRecord::Base.transaction do
+  table_names.each do |f|
+    file_path = File.join(\"\#{Rails.root.to_s}\",\"db/seeds/\#{Rails.env.to_s}/\#{f}.csv\")
+data_entry(file_path)
+  end
+
+  Dir::glob(File.join(\"\#{Rails.root.to_s}\",\"db/seeds/\#{Rails.env.to_s}/*.csv\")).each do |file_path|
+    unless table_names.index(File::basename(file_path).sub(/.csv$/,\"\"))
+      data_entry(file_path)
+    end
+  end
+
+  Dir::glob(File.join(\"\#{Rails.root.to_s}\",\"db/seeds/\#{Rails.env.to_s}/*.rb\")).each do |file_path|
+    puts file_path
+    require(file_path) if File.exist?(file_path)
+  end
+end
+EOF"
 
 # set .gitignore
 run 'rm .gitignore'
